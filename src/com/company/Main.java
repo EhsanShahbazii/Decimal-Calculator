c514737052f5cf37f1a428340904dfa83f4f0bf4
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-01-19 23:21:26 +0330

    create GUI and JFrame

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void GUI() {
+        JFrame jFrame = new JFrame();
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setLayout(null);
+        jFrame.setBounds(770, 200, 400, 600);
+
+        jFrame.setTitle("Decimal Calculator");
+        jFrame.setResizable(false);
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
